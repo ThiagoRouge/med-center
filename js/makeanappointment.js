47ba3a1cba708afc874d24af244bea7dec2032b0
ThiagoRouge <teronarray@gmail.com>
2023-06-13 02:21:23 +0500

    added kz lang

--- js/makeanappointment.js
+++ js/makeanappointment.js
@@ -42,8 +42,6 @@ makeAppointmentBtn.onclick = async function(e) {
     const currentDate = new Date();
     const currentMinutes = (currentDate.getMinutes() < 10) ? '0' + currentDate.getMinutes() : currentDate.getMinutes();
     const currentTime = new Date().getHours() + '.' + currentMinutes;
-    console.log(selectedTime.value.split(':').join('.'))
-    console.log(currentTime);
     if(nameInp.value.length != 0 && surnameInp.value.length != 0 && healthComplaints.value.length > 6 && userAddress.value.length > 6 && selectedTime != null && Number(selectedTime.value.split(':').join('.')) > Number(currentTime)) {
         e.target.disabled = true;
         await push(ref(db, "appointments"), {
